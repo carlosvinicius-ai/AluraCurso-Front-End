43d710541cd2589c114b6e5fd9f570f55d1352f5
cvinicius-ai <cvinicius32@hotmail.com>
2023-02-27 20:22:52 -0300

    add referencia

--- aluramidi/AluraMidi/main.js
+++ aluramidi/AluraMidi/main.js
@@ -1,5 +1,6 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll('.tecla');
